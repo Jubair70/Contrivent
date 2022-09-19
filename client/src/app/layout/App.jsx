572be207765c8bc33bba346f2e17cc1db68ddf08
Jubair Hossain <jubair.hossain@yahoo.com>
2022-09-19 21:55:12 +0600

    Adding Semantic UI

--- client/src/app/layout/App.jsx
+++ client/src/app/layout/App.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import 'semantic-ui-css/semantic.min.css';
+import { Button } from "semantic-ui-react";
+import EventDashboard from "../../features/events/eventDashboard/EventDashboard";
+import './styles.css';
 
 function App() {
   return (
-    <div className="App">
+    <div>
      <h1>Contrivent</h1>
+     <EventDashboard/>
     </div>
   );
 }
