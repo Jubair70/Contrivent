495df2af3d5f60e86fda280f0e8dbf1c3a85638a
Jubair Hossain <jubair.hossain@yahoo.com>
2022-09-27 16:27:25 +0600

    Event CRUD complete

--- client/src/features/events/eventForm/EventForm.jsx
+++ client/src/features/events/eventForm/EventForm.jsx
@@ -1,20 +1,35 @@
-import React, { useState } from "react";
-import { Segment, Header, Form, Button } from "semantic-ui-react";
-import cuid from "cuid";
-export default function EventForm({ setFormOpen, setEvents, createEvent }) {
-  const initialValues = {
-    title: "",
-    category: "",
-    description: "",
-    city: "",
-    venue: "",
-    date: "",
+import React, { useState } from 'react';
+import { Segment, Header, Form, Button } from 'semantic-ui-react';
+import cuid from 'cuid';
+
+export default function EventForm({
+  setFormOpen,
+  setEvents,
+  createEvent,
+  selectedEvent,
+  updateEvent,
+}) {
+  const initialValues = selectedEvent ?? {
+    title: '',
+    category: '',
+    description: '',
+    city: '',
+    venue: '',
+    date: '',
   };
 
   const [values, setValues] = useState(initialValues);
 
   function handleFormSubmit() {
-    createEvent({...values,id:cuid(), hostedBy:'Bob', attendees:[], hostPhotoURL:'/assets/user.png'});
+    selectedEvent
+      ? updateEvent({ ...selectedEvent, ...values })
+      : createEvent({
+          ...values,
+          id: cuid(),
+          hostedBy: 'Bob',
+          attendees: [],
+          hostPhotoURL: '/assets/user.png',
+        });
     setFormOpen(false);
   }
 
@@ -25,68 +40,68 @@ export default function EventForm({ setFormOpen, setEvents, createEvent }) {
 
   return (
     <Segment clearing>
-      <Header content="Create new event" />
+      <Header content={selectedEvent ? 'Edit the event' : 'Create new event'} />
       <Form onSubmit={handleFormSubmit}>
         <Form.Field>
           <input
-            type="text"
-            placeholder="Event title"
-            name="title"
+            type='text'
+            placeholder='Event title'
+            name='title'
             value={values.title}
             onChange={(e) => handleInputChange(e)}
           />
         </Form.Field>
         <Form.Field>
           <input
-            type="text"
-            placeholder="Category"
-            name="category"
+            type='text'
+            placeholder='Category'
+            name='category'
             value={values.category}
             onChange={(e) => handleInputChange(e)}
           />
         </Form.Field>
         <Form.Field>
           <input
-            type="text"
-            placeholder="Description"
-            name="description"
+            type='text'
+            placeholder='Description'
+            name='description'
             value={values.description}
             onChange={(e) => handleInputChange(e)}
           />
         </Form.Field>
         <Form.Field>
           <input
-            type="text"
-            placeholder="City"
-            name="city"
+            type='text'
+            placeholder='City'
+            name='city'
             value={values.city}
             onChange={(e) => handleInputChange(e)}
           />
         </Form.Field>
         <Form.Field>
           <input
-            type="text"
-            placeholder="Venue"
-            name="venue"
+            type='text'
+            placeholder='Venue'
+            name='venue'
             value={values.venue}
             onChange={(e) => handleInputChange(e)}
           />
         </Form.Field>
         <Form.Field>
           <input
-            type="date"
-            placeholder="Date"
-            name="date"
+            type='date'
+            placeholder='Date'
+            name='date'
             value={values.date}
             onChange={(e) => handleInputChange(e)}
           />
         </Form.Field>
-        <Button type="submit" floated="right" positive content="Submit" />
+        <Button type='submit' floated='right' positive content='Submit' />
         <Button
           onClick={() => setFormOpen(false)}
-          type="submit"
-          floated="right"
-          content="Cancel"
+          type='submit'
+          floated='right'
+          content='Cancel'
         />
       </Form>
     </Segment>
