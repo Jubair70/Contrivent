df61b993101a7b63673daf74afc8e65cf63e1ea7
Jubair Hossain <jubair.hossain@yahoo.com>
2022-09-29 13:01:48 +0600

    Added redux-devtool, modify event form

--- client/src/index.js
+++ client/src/index.js
@@ -1,28 +1,34 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import 'semantic-ui-css/semantic.min.css';
-import './app/layout/styles.css';
-import App from './app/layout/App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "semantic-ui-css/semantic.min.css";
+import "./app/layout/styles.css";
+import App from "./app/layout/App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { configureStore } from "./app/store/configureStore";
+import ScrollToTop from "./app/layout/ScrollToTop";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const store = configureStore();
 
 function render() {
-    root.render(
-        <BrowserRouter>
+  root.render(
+    <Provider store={store}>
+      <BrowserRouter>
+        <ScrollToTop />
         <App />
-        </BrowserRouter>
-    );    
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
-
-if(module.hot){
-    module.hot.accept('./app/layout/App',function () {
-        setTimeout(render);
-    })
+if (module.hot) {
+  module.hot.accept("./app/layout/App", function () {
+    setTimeout(render);
+  });
 }
-render()
+render();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
