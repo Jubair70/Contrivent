72e720af9d592dc37345a07ba4c79f433b6c5e8c
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-07 17:27:00 +0600

    final update

--- client/src/features/profiles/profileReducer.jsx
+++ client/src/features/profiles/profileReducer.jsx
@@ -1,30 +1,37 @@
 import {
-    LISTEN_TO_CURRENT_USER_PROFILE,
-    LISTEN_TO_SELECTED_USER_PROFILE,
-  } from './profileConstants';
-  
-  const initialState = {
-    currentUserProfile: null,
-    selectedUserProfile: null,
-  };
-  
-  export default function profileReducer(
-    state = initialState,
-    { type, payload }
-  ) {
-    switch (type) {
-      case LISTEN_TO_CURRENT_USER_PROFILE:
-        return {
-          ...state,
-          currentUserProfile: payload,
-        };
-      case LISTEN_TO_SELECTED_USER_PROFILE:
-        return {
-          ...state,
-          selectedUserProfile: payload,
-        };
-      default: {
-        return state;
-      }
+  LISTEN_TO_CURRENT_USER_PROFILE,
+  LISTEN_TO_SELECTED_USER_PROFILE,
+  LISTEN_TO_USER_PHOTOS,
+} from './profileConstants';
+
+const initialState = {
+  currentUserProfile: null,
+  selectedUserProfile: null,
+  photos: [],
+};
+
+export default function profileReducer(
+  state = initialState,
+  { type, payload }
+) {
+  switch (type) {
+    case LISTEN_TO_CURRENT_USER_PROFILE:
+      return {
+        ...state,
+        currentUserProfile: payload,
+      };
+    case LISTEN_TO_SELECTED_USER_PROFILE:
+      return {
+        ...state,
+        selectedUserProfile: payload,
+      };
+    case LISTEN_TO_USER_PHOTOS:
+      return {
+        ...state,
+        photos: payload,
+      };
+    default: {
+      return state;
     }
   }
+}
