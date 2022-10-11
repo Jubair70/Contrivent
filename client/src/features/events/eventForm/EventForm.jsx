9e4412b1aa27e31820fc9f27e100fdc9589bdfe0
Jubair Hossain <jubair.hossain@yahoo.com>
2022-10-11 16:24:54 +0600

    updated security rules

--- client/src/features/events/eventForm/EventForm.jsx
+++ client/src/features/events/eventForm/EventForm.jsx
@@ -34,13 +34,19 @@ export default function EventForm({ match, history }) {
     category: '',
     description: '',
     city: {
-      address: '',
-      latLng: null,
-    },
-    venue: {
-      address: '',
-      latLng: null,
-    },
+      address: 'NY, USA',
+      latlng: {
+          lat: 40.7484405,
+          lng:  -73.98566440000002
+      }
+  },
+  venue: {
+      address: 'Empire State Building, 5th Avenue, New York, NY, USA',
+      latlng: {
+          lat: 40.7484405,
+          lng:  -73.98566440000002
+      }
+  },
     date: '',
   };
 
